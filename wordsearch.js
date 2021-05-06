e33c6490741b7800fae6084e3ba9f488ec8f3c0f
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-05 20:57:42 -0600

    Word search that looks horizontally and vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
+    const transpose = function(matrix) {
+        let newArr = [];
+        for (let i = 0; i < matrix[0].length; i++) {
+          let arr = [];
+          for (let j = 0; j < matrix.length; j++) {
+            arr.push(matrix[j][i]);
+          };
+          newArr.push(arr);
+        }
+        return newArr;
+      };
+    newArr = transpose(letters);
+    const verticalJoin = newArr.map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
